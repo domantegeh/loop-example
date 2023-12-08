e3b226e6f8f70d50f75c3383b1677685d4b72faa
doman-tegeh <tegeh.add@gmail.com>
2023-12-08 10:11:46 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// for loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// for-of loop
+let myArray = ["Luke", "Han", "Chewbacca", "Leia"];
+for (const arrayItem of myArray) {
+  console.log(arrayItem);
+}
+
+// while loop
+let i = 1;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+
+// do-while loop
+let j = 1;
+do {
+  console.log(j);
+  j++;
+} while (j < 5);
